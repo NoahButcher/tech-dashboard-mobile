47a81b105e683179ff0ed2947611024ee3317705
Noah Butcher <NButcher@rhmail.org>
2020-03-17 11:45:22 -0400

    Updates

---
+++ precache-manifest.90201653888cdbaaeda1bbeee9b6f0da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bf0475987ab8d3f69639b2ddcc77cd",
+    "url": "/tech-dashboard-mobile/index.html"
+  },
+  {
+    "revision": "765a150b599951f4f62f",
+    "url": "/tech-dashboard-mobile/static/css/main.f38aaca9.chunk.css"
+  },
+  {
+    "revision": "e61a26c4378a43042a9a",
+    "url": "/tech-dashboard-mobile/static/js/2.4f7ef907.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tech-dashboard-mobile/static/js/2.4f7ef907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765a150b599951f4f62f",
+    "url": "/tech-dashboard-mobile/static/js/main.ca83ad59.chunk.js"
+  },
+  {
+    "revision": "626f4a0faf33fddb88e3",
+    "url": "/tech-dashboard-mobile/static/js/runtime-main.c368c7ba.js"
+  }
+]);
